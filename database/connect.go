d45cfe7bba86a24c6913bef50a173d8ec29f510a
minguu <minguu42@gmail.com>
2021-02-06 13:04:51 +0900

    DBとの接続ができるように変更

---
+++ database/connect.go
@@ -0,0 +1,20 @@
+package database
+
+import (
+	"database/sql"
+	"log"
+	"os"
+)
+
+var (
+	DRIVER = os.Getenv("DRIVER")
+	DATASOURCE = os.Getenv("DATASOURCE")
+)
+
+func Connect() *sql.DB {
+	db, err := sql.Open(DRIVER, DATASOURCE)
+	if err != nil {
+		log.Fatal("database connection error", err)
+	}
+	return db
+}
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"ca-game-api/database"
 	"ca-game-api/handlers"
+	_ "github.com/go-sql-driver/mysql"
 	"log"
 	"net/http"
 )
 
 func main() {
+	db := database.Connect()
+	defer db.Close()
+
+	// 通信確認用。
+	if err := db.Ping(); err != nil {
+		log.Fatal("database ping error: ", err)
+	}
+
 	http.HandleFunc("/", handlers.UserHandler)
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("server error: ", err)
