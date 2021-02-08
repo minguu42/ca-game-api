75b24c7422a37822e86aca82082d01cc5cc6aa2b
minguu <minguu42@gmail.com>
2021-02-08 20:30:49 +0900

    JSONリクエストとレスポンスをそれぞれ別の構造体にする

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -8,19 +8,11 @@ import (
 	"net/http"
 )
 
-type User struct {
+type UserCreateJsonRequest struct {
 	Name string `json:"name"`
 }
 
-type Token struct {
-	Token string `json:"token"`
-}
-
-type UserCreateRequestJson struct {
-	Name string `json:"name"`
-}
-
-type UserCreateResponseJson struct {
+type UserCreateJsonResponse struct {
 	Token string `json:"token"`
 }
 
@@ -30,33 +22,33 @@ func UserCreateHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// ボディからユーザの取得
-	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	var jsonRequest UserCreateJsonRequest
+	if err := json.NewDecoder(r.Body).Decode(&jsonRequest); err != nil {
 		log.Fatal("user decode error: ", err)
 	}
 
-	// トークンの作成
-	randomStr, err := helper.GenerateRandomString(22)
+	token, err := helper.GenerateRandomString(22)
 	if err != nil {
 		log.Fatal("token generate error: ", err)
 	}
-	token := Token{
-		Token: randomStr,
-	}
-
-	// トークンをハッシュ化し、データベースに保存する。
-	digestToken := helper.HashToken(token.Token)
+	digestToken := helper.HashToken(token)
 
 	db := database.Connect()
 	defer db.Close()
-	if err := database.InsertUser(db, user.Name, digestToken); err != nil {
+	if err := database.InsertUser(db, jsonRequest.Name, digestToken); err != nil {
 		log.Fatal("database create user error: ", err)
 	}
 
-	if err := json.NewEncoder(w).Encode(token); err != nil {
+	jsonResponse := UserCreateJsonResponse{
+		Token: token,
+	}
+	if err := json.NewEncoder(w).Encode(jsonResponse); err != nil {
 		log.Fatal("json encode error: ", err)
 	}
+}
+
+type UserGetJsonResponse struct {
+	Name string `json:"name"`
 }
 
 func UserGetHandler(w http.ResponseWriter, r *http.Request) {
@@ -75,12 +67,16 @@ func UserGetHandler(w http.ResponseWriter, r *http.Request) {
 		log.Fatal("database get user name error: ", err)
 	}
 
-	user := User{
+	jsonResponse := UserGetJsonResponse{
 		Name: name,
 	}
-	if err := json.NewEncoder(w).Encode(user); err != nil {
+	if err := json.NewEncoder(w).Encode(jsonResponse); err != nil {
 		log.Fatal("json encode error: ", err)
 	}
+}
+
+type UserUpdateJsonRequest struct {
+	Name string `json:"name"`
 }
 
 func UserUpdateHandler(w http.ResponseWriter, r *http.Request) {
@@ -91,8 +87,8 @@ func UserUpdateHandler(w http.ResponseWriter, r *http.Request) {
 
 	xToken := r.Header.Get("x-token")
 	digestXToken := helper.HashToken(xToken)
-	var user User
-	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+	var jsonRequest UserUpdateJsonRequest
+	if err := json.NewDecoder(r.Body).Decode(&jsonRequest); err != nil {
 		log.Fatal("user decode error: ", err)
 	}
 
@@ -102,7 +98,7 @@ func UserUpdateHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Fatal("database get user id error: ", err)
 	}
-	if 	err := database.UpdateUser(db, id, user.Name); err != nil {
+	if err := database.UpdateUser(db, id, jsonRequest.Name); err != nil {
 		log.Fatal("database user update error: ", err)
 	}
 }
