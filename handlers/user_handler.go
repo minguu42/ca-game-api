189d2507bb22db445c80a7296aa5f5123e802375
minguu <minguu42@gmail.com>
2021-02-08 14:02:36 +0900

    UserCreateHanlderの実装完了

--- handlers/user_handler.go
+++ handlers/user_handler.go
@@ -2,6 +2,8 @@ package handlers
 
 import (
 	"encoding/json"
+	"github.com/minguu42/ca-game-api/database"
+	"github.com/minguu42/ca-game-api/helper"
 	"log"
 	"net/http"
 )
@@ -10,10 +12,42 @@ type User struct {
 	Name string `json:"name"`
 }
 
-// 動作確認用。のちに削除する。
-func UserHandler(w http.ResponseWriter, r *http.Request) {
-	user := User{"Joseph Joestar"}
-	if err := json.NewEncoder(w).Encode(user); err != nil {
+type Token struct {
+	Token string `json:"token"`
+}
+
+func UserCreateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	// ボディからユーザの取得
+	var user User
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		log.Fatal("user decode error: ", err)
+	}
+
+	// トークンの作成
+	randomStr, err := helper.GenerateRandomString(22)
+	if err != nil {
+		log.Fatal("token generate error: ", err)
+	}
+	token := Token {
+		Token: randomStr,
+	}
+
+	// トークンをハッシュ化し、データベースに保存する。
+	digestToken := helper.HashToken(token.Token)
+
+	db := database.Connect()
+	defer db.Close()
+	if err := database.InsertUser(db, user.Name, digestToken); err != nil {
+		log.Fatal("database create user error: ", err)
+	}
+	log.Println(digestToken)
+
+	if err := json.NewEncoder(w).Encode(token); err != nil {
 		log.Fatal("json encode error: ", err)
 	}
 }
