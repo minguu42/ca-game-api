d4809140f1d69c9dfb13dcdf5d4a0f7101fbc4ab
minguu <minguu42@gmail.com>
2021-02-08 13:09:55 +0900

    不要な部分の削除

--- helper/hash_token.go
+++ helper/hash_token.go
@@ -5,7 +5,7 @@ import (
 	"encoding/hex"
 )
 
-func HashToken(token string) (string){
+func HashToken(token string) string {
 	digestTokenByte := sha256.Sum256([]byte(token))
 	digestToken := hex.EncodeToString(digestTokenByte[:])
 	return digestToken
