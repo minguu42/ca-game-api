edd0e301c8ee8f8629b77de2f08bb858be102b07
minguu <minguu42@gmail.com>
2021-02-08 12:40:56 +0900

    HashToken関数の追加

---
+++ helper/hash_token.go
@@ -0,0 +1,12 @@
+package helper
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+)
+
+func HashToken(token string) (string){
+	digestTokenByte := sha256.Sum256([]byte(token))
+	digestToken := hex.EncodeToString(digestTokenByte[:])
+	return digestToken
+}
