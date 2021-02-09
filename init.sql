d742f2cd9a5e3488f3fa70b7c92e5d95b6446a00
minguu <minguu42@gmail.com>
2021-02-09 20:44:29 +0900

    テーブルに適宜created_at, updated_atのカラムを追加

--- init.sql
+++ init.sql
@@ -1,26 +1,29 @@
 CREATE TABLE IF NOT EXISTS `users` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `name` varchar(255) UNIQUE NOT NULL,
-    `digest_token` varchar(255) NOT NULL
+    id int PRIMARY KEY AUTO_INCREMENT,
+    name varchar(255) UNIQUE NOT NULL,
+    digest_token varchar(255) NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `characters` (
-    `id` int PRIMARY KEY,
-    `name` varchar(255) NOT NULL,
-    `rarity` int NOT NULL,
-    `user_id` int NOT NULL
+    id int PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    rarity int NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS `user_ownership_characters` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `user_id` int NOT NULL,
-    `character_id` int NOT NULL
+    id int PRIMARY KEY AUTO_INCREMENT,
+    user_id int NOT NULL,
+    character_id int NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE IF NOT EXISTS `gacha_results` (
-    `id` int PRIMARY KEY AUTO_INCREMENT,
-    `user_id` int NOT NULL,
-    `character_id` int NOT NULL
+    id int PRIMARY KEY AUTO_INCREMENT,
+    user_id int NOT NULL,
+    character_id int NOT NULL,
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 ALTER TABLE `user_ownership_characters`
