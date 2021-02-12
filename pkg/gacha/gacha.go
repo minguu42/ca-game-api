3c4ebe5a55cd0f447ecd06cbda7606a60b1e958c
minguu <minguu42@gmail.com>
2021-02-12 15:42:55 +0900

    ガチャの処理をgacha.Draw関数に移動

--- pkg/gacha/gacha.go
+++ pkg/gacha/gacha.go
@@ -1,6 +1,16 @@
 package gacha
 
-import "database/sql"
+import (
+	"database/sql"
+	"github.com/minguu42/ca-game-api/pkg/character"
+	"github.com/minguu42/ca-game-api/pkg/helper"
+	"strconv"
+)
+
+type Result struct {
+	CharacterId string `json:"characterID"`
+	Name        string `json:"name"`
+}
 
 func ApplyGachaResult(db *sql.DB, userId, CharacterId int) error {
 	const insertSql = "INSERT INTO gacha_results (user_id, character_id) VALUES (?, ?)"
@@ -12,4 +22,43 @@ func ApplyGachaResult(db *sql.DB, userId, CharacterId int) error {
 		return err
 	}
 	return nil
+}
+
+func Draw(db *sql.DB, userId, times int) ([]Result, error) {
+	var results []Result
+	for i := 0; i < times; i++ {
+		rarity3CharacterNum, err := character.CountPerRarity(db, 3)
+		if err != nil {
+			return nil, err
+		}
+		rarity4CharacterNum, err := character.CountPerRarity(db, 4)
+		if err != nil {
+			return nil, err
+		}
+		rarity5CharacterNum, err := character.CountPerRarity(db, 5)
+		if err != nil {
+			return nil, err
+		}
+		var characterId int
+		switch selectedRarity := helper.SelectRarity(); selectedRarity{
+		case 3:
+			characterId = helper.SelectCharacterId(rarity3CharacterNum) +  + 30000000
+		case 4:
+			characterId = helper.SelectCharacterId(rarity4CharacterNum) + 40000000
+		case 5:
+			characterId = helper.SelectCharacterId(rarity5CharacterNum) + 50000000
+		}
+		name, err := character.GetName(db, characterId)
+		if err != nil {
+			return nil, err
+		}
+		if err := ApplyGachaResult(db, userId, characterId); err != nil {
+			return nil, err
+		}
+		results = append(results, Result{
+			CharacterId: strconv.Itoa(characterId),
+			Name:        name,
+		})
+	}
+	return results, nil
 }
--- pkg/handlers/gacha_handler.go
+++ pkg/handlers/gacha_handler.go
@@ -2,27 +2,19 @@ package handlers
 
 import (
 	"encoding/json"
-	"github.com/minguu42/ca-game-api/pkg/character"
 	"github.com/minguu42/ca-game-api/pkg/database"
 	"github.com/minguu42/ca-game-api/pkg/gacha"
-	"github.com/minguu42/ca-game-api/pkg/helper"
 	"github.com/minguu42/ca-game-api/pkg/user"
 	"log"
 	"net/http"
-	"strconv"
 )
-
-type GachaResult struct {
-	CharacterId string `json:"characterID"`
-	Name        string `json:"name"`
-}
 
 type GachaDrawJsonRequest struct {
 	Times int `json:"times"`
 }
 
 type GachaDrawJsonResponse struct {
-	Results []GachaResult `json:"results"`
+	Results []gacha.Result `json:"results"`
 }
 
 func GachaDrawHandler(w http.ResponseWriter, r *http.Request) {
@@ -30,56 +22,27 @@ func GachaDrawHandler(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusMethodNotAllowed)
 		return
 	}
-	db := database.Connect()
-	defer db.Close()
-	xToken := r.Header.Get("x-token")
-	userId, err := user.GetId(db, xToken)
-	if err != nil {
-		w.WriteHeader(http.StatusUnauthorized)
-		return
-	}
 
+	xToken := r.Header.Get("x-token")
 	var jsonRequest GachaDrawJsonRequest
 	if err := json.NewDecoder(r.Body).Decode(&jsonRequest); err != nil {
 		log.Fatal("user decode error: ", err)
 	}
 	times := jsonRequest.Times
 
-	var results []GachaResult
-	for i := 0; i < times; i++ {
-		rarity3CharacterNum, err := character.CountCharacterPerRarity(db, 3)
-		if err != nil {
-			log.Fatal("database get count error: ", err)
-		}
-		rarity4CharacterNum, err := character.CountCharacterPerRarity(db, 3)
-		if err != nil {
-			log.Fatal("database get count error: ", err)
-		}
-		rarity5CharacterNum, err := character.CountCharacterPerRarity(db, 3)
-		if err != nil {
-			log.Fatal("database get count error: ", err)
-		}
-		var selectedCharacterId int
-		switch selectedRarity := helper.SelectRarity(); selectedRarity {
-		case 3:
-			selectedCharacterId = helper.SelectCharacterId(rarity3CharacterNum) + 30000000
-		case 4:
-			selectedCharacterId = helper.SelectCharacterId(rarity4CharacterNum) + 40000000
-		case 5:
-			selectedCharacterId = helper.SelectCharacterId(rarity5CharacterNum) + 50000000
-		}
-		name, err := character.GetCharacterName(db, selectedCharacterId)
-		if err != nil {
-			log.Fatal("database get character name error: ", err)
-		}
-		if err := gacha.ApplyGachaResult(db, userId, selectedCharacterId); err != nil {
-			log.Fatal("database insert GachaResult error: ", err)
-		}
-		results = append(results, GachaResult{
-			CharacterId: strconv.Itoa(selectedCharacterId),
-			Name:        name,
-		})
+	db := database.Connect()
+	defer db.Close()
+	userId, err := user.GetId(db, xToken)
+	if err != nil {
+		w.WriteHeader(http.StatusUnauthorized)
+		return
 	}
+
+	results, err := gacha.Draw(db, userId, times)
+	if err != nil {
+		log.Fatal("gacha draw error")
+	}
+
 	jsonResponse := GachaDrawJsonResponse{
 		Results: results,
 	}
