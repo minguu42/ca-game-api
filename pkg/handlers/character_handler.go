406ef3a23b3bb13287d709e49ac2c8264433a66a
minguu <minguu42@gmail.com>
2021-02-17 10:06:47 +0900

    CharacterListHandlerにログ出力を追加

--- pkg/handlers/character_handler.go
+++ pkg/handlers/character_handler.go
@@ -13,24 +13,33 @@ type CharacterListJsonResponse struct {
 }
 
 func CharacterListHandler(w http.ResponseWriter, r *http.Request) {
+	log.Printf("INFO START %v request to %v came from %v", r.Method, r.URL, r.Header.Get("User-Agent"))
 	if r.Method != http.MethodGet {
 		w.WriteHeader(http.StatusMethodNotAllowed)
+		log.Println("ERROR Status method is not allowed")
 		return
 	}
 
 	xToken := r.Header.Get("x-token")
+	log.Println("INFO Get x-token - Success")
 
 	db := database.Connect()
 	defer db.Close()
 	characters, err := user.GetCharacterList(db, xToken)
 	if err != nil {
-		log.Fatal("get character list error: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		log.Println("ERROR Get character list error:", err)
+		return
 	}
+	log.Println("INFO Get character list - Success")
 
 	jsonResponse := CharacterListJsonResponse{
 		Characters: characters,
 	}
 	if err := json.NewEncoder(w).Encode(jsonResponse); err != nil {
-		log.Fatal("json encode error: ", err)
+		log.Println("ERROR Json encode error: ", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
+	log.Printf("INFO END %v request to %v came from %v", r.Method, r.URL, r.Header.Get("User-Agent"))
 }
