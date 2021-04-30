6e3ca49b9e85217dee8f7adffe366d39ce324205
minguu <minguu42@gmail.com>
2021-05-01 08:32:40 +0900

    User構造体を追加

--- user_model.go
+++ user_model.go
@@ -4,25 +4,32 @@ import (
 	"fmt"
 )
 
-func insertUser(name string, token string) error {
+type User struct {
+	id          int
+	name        string
+	digestToken string
+	createdAt   string
+	updatedAt   string
+}
+
+func insertUser(user User) error {
 	const createSql = `INSERT INTO users (name, digest_token) VALUES ($1, $2);`
-	digestToken := hash(token)
-	if _, err := db.Exec(createSql, name, digestToken); err != nil {
-		return fmt.Errorf("db.Exec faild: %w", err)
+	if _, err := db.Exec(createSql, user.name, user.digestToken); err != nil {
+		return fmt.Errorf("db.Exec failed: %w", err)
 	}
 	return nil
 }
 
-func selectUserByToken(token string) (string, error) {
-	const selectSql = `SELECT name FROM users WHERE digest_token = $1`
+func selectUserByToken(token string) (User, error) {
+	const selectSql = `SELECT * FROM users WHERE digest_token = $1`
 	digestToken := hash(token)
 
-	var name string
+	var user User
 	row := db.QueryRow(selectSql, digestToken)
-	if err := row.Scan(&name); err != nil {
-		return "", fmt.Errorf("row.Scan faild: %w", err)
+	if err := row.Scan(&user.id, &user.name, &user.digestToken, &user.createdAt, &user.updatedAt); err != nil {
+		return user, fmt.Errorf("row.Scan failed: %w", err)
 	}
-	return name, nil
+	return user, nil
 }
 
 func selectUserId(token string) (int, error) {
@@ -31,7 +38,7 @@ func selectUserId(token string) (int, error) {
 	row := db.QueryRow(selectSql, digestToken)
 	var id int
 	if err := row.Scan(&id); err != nil {
-		return 0, fmt.Errorf("row.Scan faild: %w", err)
+		return 0, fmt.Errorf("row.Scan failed: %w", err)
 	}
 	return id, nil
 }
@@ -41,16 +48,15 @@ func selectUserIdByUserCharacterId(userCharacterId int) (int, error) {
 	row := db.QueryRow(selectSql, userCharacterId)
 	var id int
 	if err := row.Scan(&id); err != nil {
-		return 0, fmt.Errorf("row.Scan faild: %w", err)
+		return 0, fmt.Errorf("row.Scan failed: %w", err)
 	}
 	return id, nil
 }
 
-func updateUser(token, newName string) error {
+func updateUser(user User) error {
 	const updateSql = `UPDATE users SET name = $1 WHERE digest_token = $2`
-	digestToken := hash(token)
-	if _, err := db.Exec(updateSql, newName, digestToken); err != nil {
-		return fmt.Errorf("db.Exec faild: %w", err)
+	if _, err := db.Exec(updateSql, user.name, user.digestToken); err != nil {
+		return fmt.Errorf("db.Exec failed: %w", err)
 	}
 	return nil
 }
