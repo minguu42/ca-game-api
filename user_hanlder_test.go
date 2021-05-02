280a537278d015f40c29e0607b564733fda790d4
minguu <minguu42@gmail.com>
2021-05-02 17:41:52 +0900

    TestGetUser関数の追加

--- user_hanlder_test.go
+++ user_hanlder_test.go
@@ -27,6 +27,7 @@ func setUp() {
 	var err error
 	mux = http.NewServeMux()
 	mux.HandleFunc("/user/create", PostUser)
+	mux.HandleFunc("/user/get", GetUser)
 	writer = httptest.NewRecorder()
 	db, err = sql.Open("postgres", "postgres://test:password@localhost:15432/ca_game_api_db_test?sslmode=disable")
 	if err != nil {
@@ -55,5 +56,24 @@ func TestPostUser(t *testing.T) {
 	}
 	if response.Token == "" {
 		t.Errorf("token is none")
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	writer = httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/user/get", nil)
+	request.Header.Set("x-token", "ceKeMPeYr0eF3K5e4Lfjfe")
+	mux.ServeHTTP(writer, request)
+
+	if writer.Code != 200 {
+		t.Errorf("Response code is %v", writer.Code)
+	}
+
+	var response GetUserResponse
+	if err := json.Unmarshal(writer.Body.Bytes(), &response); err != nil {
+		log.Println("json.Unmarshal failed:", err)
+	}
+	if response.Name != "test user" {
+		t.Errorf("user name is %v", response.Name)
 	}
 }
