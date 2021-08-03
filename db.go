c9f6183c18e72a3ddae31261e21d149888412bbb
minguu <minguu42@gmail.com>
2021-08-03 20:58:33 +0900

    logging 関数で表示するログを変更

--- db.go
+++ db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"time"
 )
 
 var db *sql.DB
@@ -12,12 +13,27 @@ func OpenDb() {
 	var err error
 	db, err = sql.Open("postgres", os.Getenv("DSN"))
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal("open db failed:", err)
+	}
+
+	isDBReady := false
+	failureTimes := 0
+	for !isDBReady {
+		err := db.Ping()
+		if err == nil {
+			isDBReady = true
+		} else if failureTimes < 2 {
+			log.Println("ping db failed. try again")
+			time.Sleep(time.Second * 15)
+			failureTimes += 1
+		} else {
+			log.Fatal("ping db failed:", err)
+		}
 	}
 }
 
 func CloseDb() {
 	if err := db.Close(); err != nil {
-		log.Fatal(err)
+		log.Fatal("close db failed:", err)
 	}
 }
